db768de4510593d5aadb0eecca4cda7f4201901a
siam-ahmed27 <siamhossain725@gmail.com>
2024-03-03 19:04:11 +0600

    Posts Left Side Finished

--- js/script.js
+++ js/script.js
@@ -18,38 +18,33 @@ const displayPosts = (posts) => {
 
     postCard.innerHTML = `
     <div>
-            <!-- image -->
             <div class="indicator">
               <span class="indicator-item badge badge-secondary"></span>
               <div class="grid w-[85px] h-[85px] bg-base-300 place-items-center rounded-2xl">
-                <img src="${posts.image}">
+                <img class="w-full rounded-2xl object-cover" src="${phone.image}">
               </div>
             </div>
           </div>
           <div class="space-y-3">
             <div class="flex gap-5 font-inter font-medium text-sm text-[#12132DCC]">
-              <span>#Music</span>
-              <p>Author: <span>${posts.author?.name}</span></p>
+              <span>#<span>${phone.category}</span></span>
+              <p>Author: <span>${phone.author?.name}</span></p>
             </div>
-            <h3 class="text-lg lg:text-xl font-bold ">10 Kids Unaware of Their Halloween Costume</h3>
-            <p class="font-inter text-dark-color font-medium">It’s one thing to subject yourself to
-              ha Halloween costume
-              mishap
-              because, hey that’s your prerogative
-            </p>
+            <h3 class="text-lg lg:text-xl font-bold ">${phone.title}</h3>
+            <p class="font-inter text-dark-color font-medium">${phone.description}</p>
             <hr class="border-[1.5px] border-dashed">
             <div class="flex gap-6 font-inter text-dark-color font-medium">
               <div class="flex gap-2 items-center">
                 <img src="images/messege.svg">
-                <span>560</span>
+                <span>${phone.comment_count}</span>
               </div>
               <div class="flex gap-2 items-center">
                 <img src="images/eye.svg">
-                <span>1,568</span>
+                <span>${phone.view_count}</span>
               </div>
               <div class="flex gap-2 items-center">
                 <img src="images/clock.svg">
-                <span>5 min</span>
+                <span>${phone.posted_time}</span>
               </div>
             </div>
           </div>
